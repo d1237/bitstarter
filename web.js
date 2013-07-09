50a75b4bfd6c03c36068a1893cc35097c8695119
Danilo <danilo.marcolongo@gmail.com>
2013-07-09 05:26:45 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+//var fr = require('fs')
+
+st = fs.readFileSync('index.html');
+st = buf.toString(st);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(st);
 });
 
 var port = process.env.PORT || 5000;
